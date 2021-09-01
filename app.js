4669d6cfa0190250b8f1fe78b8445189afae34b0
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2021-09-01 15:41:39 +0530

    cash manager app is created

---
+++ app.js
@@ -0,0 +1,47 @@
+const billAmount  = document.querySelector("#bill-amount");
+const cashGiven   = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message     = document.querySelector("#error-message");
+const noOfNotes   = document.querySelectorAll(".no-of-notes");
+
+const notes =[2000,500,100,20,10,5,1];
+
+
+
+checkButton.addEventListener("click",function validateBillandCashAmount()
+{
+   message.style.display = "none";
+  if(billAmount.value>0)
+  {
+    if(cashGiven.value >= billAmount.value)
+    {
+        const amountToBeReturned = cashGiven.value - billAmount.value ;
+        calculateChange(amountToBeReturned);
+    }
+    else{
+          errorMessage("are you comedy me?");
+    }
+  }
+  else
+  {
+    errorMessage("Bill should be greater than 0");
+  }
+});
+
+function errorMessage(_something)
+{
+   message.style.display = "block";
+  message.innerText = _something;
+}
+
+function calculateChange(amount)
+{
+ let i=0;
+ while (i<notes.length) {
+   const numberOfNotes = Math.trunc(amount/notes[i]);
+   amount %= notes[i];
+   noOfNotes[i].innerText = numberOfNotes;
+   i++;
+ }
+}
+
